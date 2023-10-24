efc21844140fc073db63816609ac91913276fd35
Igor M <beerk0ff@yandex.ru>
2023-10-24 16:31:49 +0600

    1 thread explicit

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,8 @@ void StartServer(const cmd_parser::Args& args) {
     app::AppConfig conf {
         .db_url = GetDbURLFromEnv(),
         // .num_threads = std::thread::hardware_concurrency()
-        .num_threads = 2 // in cloud we have only 2 threads
+        // .num_threads = 2 // in cloud we have only 2 threads
+        .num_threads = 1
     };
     app::Application app(game, conf);
 
