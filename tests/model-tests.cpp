baf0f95bba4e0e337708cb2ad92d930da1f44d7c
Igor M <beerk0ff@yandex.ru>
2023-10-24 21:12:48 +0600

    tests fix

--- tests/model-tests.cpp
+++ tests/model-tests.cpp
@@ -15,7 +15,8 @@ SCENARIO("Loot spawn") {
         map.AddLootTypeWorth(1);
         Road road(Road::HORIZONTAL, {0 ,0}, 10);
         map.AddRoad(road);
-        GameSession session(&map, 0, false, {5s, 1.0}, 1000, {});
+        GameSession session(&map, 0, false, {3s, 1.0}, 60000, {});
+        session.DoOnRetire([](Dog::Id dog, const Map::Id&){});
         WHEN("Playerd joined and enough time passed") {
             auto* dog = session.NewDog("dog"s);
             session.OnTick(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::duration<size_t, std::milli>(5000)));
